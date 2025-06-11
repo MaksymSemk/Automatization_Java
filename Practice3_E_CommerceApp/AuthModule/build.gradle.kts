e16456bfdd613400ef7e10a153a23aa975c80e6a
MaksymSemk <maksumsemen2006@gmail.com>
2025-06-11 22:46:55 +0300

    Created simple E-commence market cli with authorization and updated gitignore

--- Practice3_E_CommerceApp/AuthModule/build.gradle.kts
+++ Practice3_E_CommerceApp/AuthModule/build.gradle.kts
@@ -12,6 +12,9 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("io.jsonwebtoken:jjwt-api:0.12.3")
+    runtimeOnly ("io.jsonwebtoken:jjwt-impl:0.12.3")
+    runtimeOnly ("io.jsonwebtoken:jjwt-jackson:0.12.3")
 }
 
 tasks.test {
