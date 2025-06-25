c7f8711aa1068353f80fc7ad60ca5b18511c3304
MaksymSemk <maksumsemen2006@gmail.com>
2025-06-25 22:49:41 +0300

    removed unnecessary import

--- Practice5_E_CommerceAppWithTesting/AuthModule/src/test/java/org/example/RegistrationTest.java
+++ Practice5_E_CommerceAppWithTesting/AuthModule/src/test/java/org/example/RegistrationTest.java
@@ -1,7 +1,5 @@
 package org.example;
 
-
-import org.junit.jupiter.api.Test;
 import org.junit.platform.suite.api.IncludeTags;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
